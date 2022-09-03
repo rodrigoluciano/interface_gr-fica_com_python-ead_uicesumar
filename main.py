bd541fb42ebbf3c1b9d04acd41d4eded32454478
rodrigo <rodrigolucianocosta@gmail.com>
2022-09-03 19:58:47 -0300

    interconnect interface with database

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
+import sqlite3
 
 
 class Login(QDialog):
     def __init__(self):
         super(Login, self).__init__()
-        loadUi("login.ui", self)
+        loadUi("interface/login.ui", self)
         self.loginbutton.clicked.connect(self.login_function)
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
         self.createsignupactbutton.clicked.connect(self.gotocreate)
@@ -17,6 +18,22 @@ class Login(QDialog):
         password = self.password.text()
         print("sucessfull loged in email: ", email, "and password:", password)
 
+        '''verify if blank'''
+        if len(email)==0 or len(password)==0:
+            self.error.setText("Please input all fields \n"
+                               " or create account")
+        else:
+            conn = sqlite3.connect("database/client.db")
+            cur = conn.cursor()
+            query = 'SELECT password FROM login_info WHERE email =\''+email+"\'"
+            cur.execute(query)
+            result_pass = cur.fetchone()[0]
+            if result_pass == password:
+                print("Sussesfull logged")
+                self.error.setText("")
+            else:
+                self.error.setText("Invalid email or password")
+
     def gotocreate(self):
         createac = CreateAct()
         widget.addWidget(createac)
@@ -25,7 +42,7 @@ class Login(QDialog):
 class CreateAct(QDialog):
     def __init__(self):
         super(CreateAct, self).__init__()
-        loadUi("createact.ui",self)
+        loadUi("interface/createact.ui",self)
         self.signupbutton.clicked.connect(self.createactfunction)
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
         self.confirmpass.setEchoMode(QtWidgets.QLineEdit.Password)
